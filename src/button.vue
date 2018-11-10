809a9f3e942f2a2c967f1e96dc1f063f8840b2c1
Allen <yuyuye958@qq.com>
2018-11-10 18:09:31 +0800

    icon组件

--- src/button.vue
+++ src/button.vue
@@ -1,8 +1,6 @@
 <template>
     <button class="g-button" :class="{[`icon-${iconPosition}`]: true}">
-        <svg v-if="icon" class="icon" aria-hidden="true">
-            <use :xlink:href=`#icon-${icon}`></use>
-        </svg>
+        <g-icon class="icon" v-if="icon" :name="icon"></g-icon>
         <div class="content">
             <slot></slot>
         </div>
