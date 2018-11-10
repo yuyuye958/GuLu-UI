51a109ac90669b6e5f4b59d5186069f92784de98
Allen <yuyuye958@qq.com>
2018-11-10 22:33:49 +0800

    添加click事件

--- src/button.vue
+++ src/button.vue
@@ -1,7 +1,7 @@
 <template>
-    <button class="g-button" :class="{[`icon-${iconPosition}`]: true}">
-        <g-icon class="icon" v-if="icon" :name="icon"></g-icon>
-        <g-icon class="loading" name="loading"></g-icon>
+    <button class="g-button" :class="{[`icon-${iconPosition}`]: true}" @click="$emit('click')">
+        <g-icon class="icon" v-if="icon && !loading" :name="icon"></g-icon>
+        <g-icon class="icon loading" v-if="loading" name="loading"></g-icon>
         <div class="content">
             <slot></slot>
         </div>
@@ -11,6 +11,10 @@
     export default {
         props: {
             icon: {},
+            loading: {
+                type: Boolean,
+                default: false
+            },
             iconPosition: {
                 type: String,
                 default: 'left',
@@ -68,7 +72,7 @@
                 order: 1;
             }
         }
-        .loading{
+        .loading {
             animation: spin 1.8s infinite linear;
         }
     }
