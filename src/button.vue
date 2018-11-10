d275c024bcdb3a8a14ec12ba311ac36a11f48843
Allen <yuyuye958@qq.com>
2018-11-10 19:26:07 +0800

    添加loading的icon

--- src/button.vue
+++ src/button.vue
@@ -1,6 +1,7 @@
 <template>
     <button class="g-button" :class="{[`icon-${iconPosition}`]: true}">
         <g-icon class="icon" v-if="icon" :name="icon"></g-icon>
+        <g-icon class="loading" name="loading"></g-icon>
         <div class="content">
             <slot></slot>
         </div>
@@ -21,6 +22,15 @@
     }
 </script>
 <style lang="scss">
+    @keyframes spin {
+        0% {
+            transform: rotate(0deg);
+        }
+        100% {
+            transform: rotate(360deg);
+        }
+    }
+
     .g-button {
         font-size: var(--font-size);
         height: var(--button-height);
@@ -57,6 +67,9 @@
             .content {
                 order: 1;
             }
+        }
+        .loading{
+            animation: spin 1.8s infinite linear;
         }
     }
 </style>
