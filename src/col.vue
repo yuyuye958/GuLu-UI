4812ccbba05a2255a45889f5911ac353448d3dd2
Allen <yuyuye958@qq.com>
2018-11-17 22:20:38 +0800

    添加 col 和 row 组件

---
+++ src/col.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="col" :class="[`col-${span}`]">
+        <slot></slot>
+    </div>
+</template>
+<script>
+    export default {
+        name: 'GuLu-Col',
+        props: {
+            span: {
+                type: [Number, String]
+            },
+        }
+
+    }
+</script>
+<style lang="scss" scoped>
+    .col {
+        height: 100px;
+        background: grey;
+        width: 50%;
+        border: 1px solid red;
+
+        $class: col-;
+        @for $n from 1 through 24 {
+            &.#{$class}#{$n} {
+                width: ($n / 24) * 100%;
+            }
+        }
+    }
+</style>
