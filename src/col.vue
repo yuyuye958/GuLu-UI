f55a75c5d8eaf639639820530e377107be6e51bf
Allen <yuyuye958@qq.com>
2018-11-18 01:51:14 +0800

    为 col 和 row 组件添加 gutter，实现栅格布局的功能

--- src/col.vue
+++ src/col.vue
@@ -1,6 +1,9 @@
 <template>
-    <div class="col" :class="[`col-${span}`]">
-        <slot></slot>
+    <div class="col" :class="[span && `col-${span}`, offset && `offset-${offset}`]"
+         :style="{paddingLeft: gutter/2+'px', paddingRight: gutter/2+'px'}">
+        <div style="height: 100px;border: 1px solid red;">
+            <slot></slot>
+        </div>
     </div>
 </template>
 <script>
@@ -10,21 +13,30 @@
             span: {
                 type: [Number, String]
             },
+            offset: {
+                type: [Number, String]
+            }
+        },
+        data() {
+            return {
+                gutter: 0
+            }
         }
-
     }
 </script>
 <style lang="scss" scoped>
     .col {
-        height: 100px;
-        background: grey;
         width: 50%;
-        border: 1px solid red;
-
         $class: col-;
         @for $n from 1 through 24 {
             &.#{$class}#{$n} {
                 width: ($n / 24) * 100%;
+            }
+        }
+        $class-prefix: offset-;
+        @for $n from 1 through 24 {
+            &.#{$class-prefix}#{$n} {
+                margin-left: ($n / 24) * 100%;
             }
         }
     }
