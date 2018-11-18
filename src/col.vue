dba30c18b02b2b80f1edbaf048201e1347db5fb0
Allen <yuyuye958@qq.com>
2018-11-18 16:16:43 +0800

    重构 row 和 col 组件的代码

--- src/col.vue
+++ src/col.vue
@@ -1,9 +1,6 @@
 <template>
-    <div class="col" :class="[span && `col-${span}`, offset && `offset-${offset}`]"
-         :style="{paddingLeft: gutter/2+'px', paddingRight: gutter/2+'px'}">
-        <div style="height: 100px;border: 1px solid red;">
-            <slot></slot>
-        </div>
+    <div class="col" :class="colClass" :style="colStyle">
+        <slot></slot>
     </div>
 </template>
 <script>
@@ -20,6 +17,18 @@
         data() {
             return {
                 gutter: 0
+            }
+        },
+        computed: {
+            colClass() {
+                let {span, offset} = this
+                return [span && `col-${span}`, offset && `offset-${offset}`]
+            },
+            colStyle() {
+                return {
+                    paddingLeft: this.gutter / 2 + 'px',
+                    paddingRight: this.gutter / 2 + 'px'
+                }
             }
         }
     }
