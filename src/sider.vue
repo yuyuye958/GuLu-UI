bf40e4557949561a13db4dd20c32fc4ec289add2
Allen <yuyuye958@qq.com>
2018-11-20 18:05:53 +0800

    添加布局组件

---
+++ src/sider.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="sider">
+        <slot></slot>
+    </div>
+</template>
+<script>
+    export default {
+        name: 'GuLu-Sider'
+    }
+</script>
+<style lang="scss" scoped>
+
+</style>
