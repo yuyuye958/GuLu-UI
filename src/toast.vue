13eb4147931254ee4120f38f33b7392a05a102dd
Allen <yuyuye958@qq.com>
2018-11-21 23:16:59 +0800

    添加 toast 的动画效果

--- src/toast.vue
+++ src/toast.vue
@@ -1,13 +1,15 @@
 <template>
-    <div class="toast" ref="toast" :class="toastPosition">
-        <div class="toastMessage">
-            <slot v-if="!enableHtml"></slot>
-            <div v-else v-html="$slots.default[0]"></div>
+    <div class="toastWrapper" :class="toastPosition">
+        <div class="toast" ref="toast">
+            <div class="toastMessage">
+                <slot v-if="!enableHtml"></slot>
+                <div v-else v-html="$slots.default[0]"></div>
+            </div>
+            <div class="toastLine" ref="toastLine"></div>
+            <span class="toastClose" v-if="closeButton" @click="onClickClose">
+                {{closeButton.text}}
+            </span>
         </div>
-        <div class="toastLine" ref="toastLine"></div>
-        <span class="toastClose" v-if="closeButton" @click="onClickClose">
-            {{closeButton.text}}
-        </span>
     </div>
 </template>
 <script>
@@ -85,16 +87,76 @@
     $font-size: 14px;
     $toast-min-height: 40px;
     $toast-bg: rgba(0, 0, 0, 0.75);
+    @keyframes slide-up {
+        0% {
+            opacity: 0;
+            transform: translateY(100%);
+        }
+        100% {
+            opacity: 1;
+            transform: translateY(0);
+        }
+    }
+
+    @keyframes slide-down {
+        0% {
+            opacity: 0;
+            transform: translateY(-100%);
+        }
+        100% {
+            opacity: 1;
+            transform: translateY(0);
+        }
+    }
+
+    @keyframes fade-in {
+        0% {
+            opacity: 0;
+        }
+        100% {
+            opacity: 1;
+        }
+    }
+
+    .toastWrapper {
+        $animation-duration: .3s;
+        position: fixed;
+        left: 50%;
+        transform: translateX(-50%);
+        &.toast-position-top {
+            top: 0;
+            .toast {
+                border-top-left-radius: 0;
+                border-top-right-radius: 0;
+                animation: slide-down $animation-duration;
+            }
+        }
+        &.toast-position-middle {
+            top: 50%;
+            transform: translateX(-50%) translateY(-50%);
+            .toast {
+                animation: fade-in $animation-duration;
+            }
+        }
+        &.toast-position-bottom {
+            bottom: 0;
+            .toast {
+                border-bottom-left-radius: 0;
+                border-bottom-right-radius: 0;
+                animation: slide-up $animation-duration;
+            }
+        }
+    }
+
     .toast {
         font-size: $font-size;
         min-height: $toast-min-height;
         line-height: 1.8;
         background-color: $toast-bg;
         box-shadow: 0 0 3px 0 rgba(0, 0, 0, 0.5);
+        border-radius: 4px;
         color: white;
         padding: 0 16px;
-        position: fixed;
-        left: 50%;
         display: flex;
         align-items: center;
         .toastMessage {
@@ -107,18 +169,6 @@
         .toastLine {
             border-left: 1px solid #666;
             margin: 0 16px;
-        }
-        &.toast-position-top {
-            top: 0;
-            transform: translateX(-50%);
-        }
-        &.toast-position-middle {
-            top: 50%;
-            transform: translate(-50%, -50%);
-        }
-        &.toast-position-bottom {
-            bottom: 0;
-            transform: translateX(-50%);
         }
     }
 </style>
