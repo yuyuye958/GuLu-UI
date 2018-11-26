6b6dff4ea73d11c821651fdd4e5c1d0e42ed3110
Allen <yuyuye958@qq.com>
2018-11-26 15:18:55 +0800

    完成 collapse 组件的基本样式和功能

---
+++ src/collapse-item.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="collapse-item" @click="open=!open">
+        <div class="title">
+            {{title}}
+        </div>
+        <div class="content" v-if="open">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+<script>
+    export default {
+        name: "GuLu-Collapse-Item",
+        props: {
+            title: {
+                type: String,
+                required: true
+            }
+        },
+        data() {
+            return {
+                open: false
+            }
+        }
+    }
+</script>
+<style lang="scss" scoped>
+    $item-border-color: #ddd;
+    $border-radius: 4px;
+    .collapse-item {
+        margin-top: -1px;
+        .title {
+            border: 1px solid $item-border-color;
+            margin-left: -1px;
+            margin-right: -1px;
+            padding: 0 14px;
+            min-height: 32px;
+            display: flex;
+            align-items: center;
+        }
+        .content {
+            color: #a2a2a2;
+            padding: 0 14px;
+            min-height: 32px;
+            display: flex;
+            align-items: center;
+        }
+        &:first-child {
+            .title {
+                border-top-left-radius: $border-radius;
+                border-top-right-radius: $border-radius;
+            }
+        }
+        &:last-child {
+            .title:last-child {
+                border-bottom-left-radius: $border-radius;
+                border-bottom-right-radius: $border-radius;
+                margin-bottom: -1px;
+            }
+        }
+    }
+</style>
---
+++ src/collapse.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="collapse">
+        <slot></slot>
+    </div>
+</template>
+<script>
+    export default {
+        name: "GuLu-Collapse",
+
+    }
+</script>
+<style lang="scss" scoped>
+    $border-color: #ddd;
+    $border-radius: 4px;
+    .collapse {
+        border: 1px solid $border-color;
+        border-radius: $border-radius;
+    }
+</style>
